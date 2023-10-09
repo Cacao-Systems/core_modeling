688cbf5906d02023c0ade1f318a5518a886b4dbe
meena-abdelmaseeh-cacao <meena.abdelmaseeh@cacao.systems>
2023-10-09 20:55:29 +0000

    added learning_metrics

---
+++ lib/learning_metrics.py
@@ -0,0 +1,13 @@
+import torch
+def md_classification_accuracy (y_hat, y, ignore_index = None):
+    """calculate accuracy of estimates"""
+    with torch.no_grad():
+        y_hat_top_1 = torch.argmax(y_hat,dim=1)
+        y_hat_top_1 = y_hat_top_1.flatten()
+        y = y.flatten()
+        if ignore_index is not None:
+            ignore_index_fltr = [y != ignore_index]
+            y = y[ignore_index_fltr]
+            y_hat_top_1 = y_hat_top_1[ignore_index_fltr]
+        correct = torch.eq(y,y_hat_top_1).float().sum()
+        return correct/torch.numel(y)
